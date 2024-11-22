f8192b2b7dd4e94565469b26f206c619ec571502
Zahra(Shima) Khodabandeh Samani <53971515+shimakhs@users.noreply.github.com>
2024-11-22 20:40:22 +0330

    main_Ellipse_detection.m

--- main_Ellipse_detection.m
+++ main_Ellipse_detection.m
@@ -1,167 +1,132 @@
-%%% Here, it is assumed that the exact
-%%% radius and number of the desire ellipses are known and we just need to
-%%% find the location of the ellipses and angle of them.
-%  written by Zahra Khodabandeh,
-%  Isfahan University of Medical Sciences, Isfahan, Iran
-%  Email: shimakhodabandeh@yahoo.com
+%--------------------------------------------------------------------------
+% Input:
+% - E5.JPG: The input image containing ellipses to be detected.
 %
-% If you use the code provided here, please cite the following paper:
-% Khodabandeh Z, Rabbani H, Dehnavi AM, Sarrafzadeh O. The ellipselet transform. J Med Sign Sens 2019;9:145-57.
+% Output:
+% - Detected_Ellipse: The detected ellipse overlaid on the original image.
+%
+% Description:
+% This code applies the Ellipselet Transform to detect ellipses in an image.
+% It identifies ellipse parameters including center, orientation, and dimensions.
+%
+% Author:
+% Zahra Khodabandeh,
+% Isfahan University of Medical Sciences, Isfahan, Iran
+% Email: shimakhodabandeh@yahoo.com
+%
+% Reference:
+% Khodabandeh Z, Rabbani H, Dehnavi AM, Sarrafzadeh O. The ellipselet transform.
+% J Med Sign Sens 2019;9:145-57.
+%--------------------------------------------------------------------------
 
 clear all; close all; clc;
-%% read the image
-I = imread('E5.JPG');
-f = rgb2gray(I);
-f = im2double(f);
-figure; imshow(f);
-%% apply the Ellipselet transform
-basis = cell(1,4);
-N = 4;                      % number of filters
-a1 = 50;
-b1 = 20;
-% [Cle,Gke]= fde(f, 4, a, b, 'abs');
-[Cle1,Gke1]= fde1(f, 4, a1, b1, 'abs');
-[Cle2,Gke2]= fde2(f, 4, a1, b1, 'abs');
-[Cle3,Gke3]= fde3(f, 4, a1, b1, 'abs');
-[Cle4,Gke4]= fde4(f, 4, a1, b1, 'abs');
-basis{1,1}{1,1} =Cle1; 
-basis{1,1}{1,2} =Gke1; 
-basis{1,2}{1,1} =Cle2; 
-basis{1,2}{1,2} =Gke2; 
-basis{1,3}{1,1} =Cle3;
-basis{1,3}{1,2} =Gke3;
-basis{1,4}{1,1} =Cle4;
-basis{1,4}{1,2} =Gke4;
-%% draw all subbands
+
+%% Step 1: Read the Input Image
+I = imread('E5.JPG'); % Load the input image.
+f = rgb2gray(I);      % Convert the image to grayscale.
+f = im2double(f);     % Normalize the image to the [0, 1] range.
+figure; imshow(f);    % Display the grayscale image.
+
+%% Step 2: Apply the Ellipselet Transform
+% Define the number of filters and ellipse dimensions.
+N = 4;  % Number of filters.
+a1 = 50; % Semi-major axis of the ellipses.
+b1 = 20; % Semi-minor axis of the ellipses.
+
+% Apply the Ellipselet Transform using custom functions (fde1, fde2, etc.).
+[Cle1, Gke1] = fde1(f, N, a1, b1, 'abs');
+[Cle2, Gke2] = fde2(f, N, a1, b1, 'abs');
+[Cle3, Gke3] = fde3(f, N, a1, b1, 'abs');
+[Cle4, Gke4] = fde4(f, N, a1, b1, 'abs');
+
+% Store the transform results in a cell array for organization.
+basis = cell(1, 4);
+basis{1, 1} = {Cle1, Gke1};
+basis{1, 2} = {Cle2, Gke2};
+basis{1, 3} = {Cle3, Gke3};
+basis{1, 4} = {Cle4, Gke4};
+
+%% Step 3: Process Subbands and Generate Binary Masks
+% Create binary masks for each subband and visualize them.
 figure;
-k5 = 1;
-for i = 1: size(basis,1)
-    for j = 1:size(basis,2)
-            k = 1;
-        for i1 = 1:size(Cle1,1)
-            for j1 = 1:size(Cle1,2)
-%% on binary of subbands
-    nn{i,j}{1,1}{i1,j1} =((basis{i,j}{1,1}{i1,j1})-min(basis{i,j}{1,1}{i1,j1}(:)))/(max(basis{i,j}{1,1}{i1,j1}(:))-min(basis{i,j}{1,1}{i1,j1}(:)));
-    mm{i,j}{1,1}{i1,j1}= im2bw(nn{i,j}{1,1}{i1,j1},0.2);
-    se = strel('disk',25);
-    kk{i,j}{1,1}{i1,j1}= imclose(mm{i,j}{1,1}{i1,j1},se); 
-    subplot(4,4,k5);imshow(kk{i,j}{1,1}{i1,j1});
-    k = k+1;
-    k5 = k5+1;
-    end
+k5 = 1; % Subplot index.
+for i = 1:size(basis, 1)
+    for j = 1:size(basis, 2)
+        for i1 = 1:size(Cle1, 1)
+            for j1 = 1:size(Cle1, 2)
+                % Normalize, binarize, and apply morphological closing.
+                normalized = (basis{i, j}{1, 1}{i1, j1} - min(basis{i, j}{1, 1}{i1, j1}(:))) ...
+                              / (max(basis{i, j}{1, 1}{i1, j1}(:)) - min(basis{i, j}{1, 1}{i1, j1}(:)));
+                binary = im2bw(normalized, 0.2);
+                se = strel('disk', 25);
+                closed = imclose(binary, se);
+
+                % Store and visualize the processed subbands.
+                kk{i, j}{1, 1}{i1, j1} = closed;
+                subplot(4, 4, k5); imshow(closed);
+                k5 = k5 + 1;
+            end
         end
     end
 end
+
+%% Step 4: Analyze Orientation Using Radon Transform
+% Analyze the orientation of detected features using Radon Transform.
 figure;
-g = 1;
-for t= 1:4
-    R1{1,t} = radon(kk{1,1}{1,1}{1,t},90);
-    med1{1,t} = medfilt1(R1{1,t});
-    subplot(1,4,g);plot(med1{1,t});
-    pks1{1,t} = findpeaks(med1{1,t});
-    maxxx1{1,t} = max(med1{1,t});
-    Th1{1,t} = 0.65*(max(pks1{1,t}));
-    A1{1,t} = med1{1,t} >=Th1{1,t};
-    MM1{1,t} = find(A1{1,t} == 1);
-if (med1{1,t}(min(MM1{1,t}):max(MM1{1,t})) >= Th1{1,t}) 
-    Theta1{1,t} = 90;
-else
-    Theta1{1,t} = {'-'};
-end
-g = g+1;
-end
-figure;
-g = 1;
-for t= 1:4
-    R2{1,t} = radon(kk{1,2}{1,1}{1,t},0);
-        med2{1,t} = medfilt1(R2{1,t});
-    subplot(1,4,g);plot(med2{1,t});
-        pks2{1,t} = findpeaks(med2{1,t});
-        maxxx2{1,t} = max(med2{1,t});
-        Th2{1,t} = 0.65*(max(pks2{1,t}));
-    A2{1,t} = med2{1,t} >=Th2{1,t};
-    MM2{1,t} = find(A2{1,t} == 1);
-if (med2{1,t}(min(MM2{1,t}):max(MM2{1,t})) >= Th2{1,t}) 
-    Theta2{1,t} = 0;
-else
-    Theta2{1,t} = {'-'};
-end
-g = g+1;
-end
-figure;
-g = 1;
-for t= 1:4
-    R3{1,t} = radon(kk{1,3}{1,1}{1,t},+45);
-        med3{1,t} = medfilt1(R3{1,t});
-    subplot(1,4,g);plot(med3{1,t});
-        pks3{1,t} = findpeaks(med3{1,t});
-        maxxx3{1,t} = max(med3{1,t});
-        Th3{1,t} = 0.65*(max(pks3{1,t}));
-    A3{1,t} = med3{1,t} >=Th3{1,t};
-    MM3{1,t} = find(A3{1,t} == 1);
-if (med3{1,t}(min(MM3{1,t}):max(MM3{1,t})) >= Th3{1,t}) 
-    Theta3{1,t} = 45;
-else
-    Theta3{1,t} = {'-'};
-end
-g = g+1;
-end
-figure;
-g = 1;
-for t= 1:4
-    R4{1,t} = radon(kk{1,4}{1,1}{1,t},-45);
-        med4{1,t} = medfilt1(R4{1,t});
-    subplot(1,4,g);plot(med4{1,t});
-        pks4{1,t} = findpeaks(med4{1,t});
-        maxxx4{1,t} = max(med4{1,t})
-                Th4{1,t} = 0.65*(max(pks4{1,t}));
-    A4{1,t} = med4{1,t} >=Th4{1,t};
-    MM4{1,t} = find(A4{1,t} == 1);
-if (med4{1,t}(min(MM4{1,t}):max(MM4{1,t})) >= Th4{1,t}) 
-    Theta4{1,t} = -45;
-else
-    Theta4{1,t} = {'-'};
-end
-g = g+1;
+angles = [90, 0, 45, -45]; % Predefined angles.
+Theta = cell(1, 4);
+for t = 1:4
+    subplot(1, 4, t);
+    R = radon(kk{1, t}{1, 1}{1, t}, angles(t));
+    med_filtered = medfilt1(R);
+    plot(med_filtered);
+
+    % Detect peaks and determine orientation threshold.
+    peaks = findpeaks(med_filtered);
+    max_peak = max(med_filtered);
+    threshold = 0.65 * max_peak;
+    above_threshold = med_filtered >= threshold;
+
+    % Determine the orientation based on peak properties.
+    if any(above_threshold)
+        Theta{t} = angles(t);
+    else
+        Theta{t} = 'N/A';
+    end
 end
 
-%% find two peaks of cofficients to find center of ellipse
-AA = cell2mat(basis{1,4}{1,1}(1,1)); % it is for -45 basis and for other degrees the basis should be changed
-figure; plot(AA);
-Th = 0.8*(max(max(AA)));
-[rowCle,ColCle] = find(AA >= Th);
+%% Step 5: Detect Ellipse Center and Dimensions
+% Combine subband information to find the ellipse center.
+AA = cell2mat(basis{1, 4}{1, 1}(1, 1)); % Use -45 degree basis.
+Th = 0.8 * max(AA(:));
+[rowCle, colCle] = find(AA >= Th);
+
+% Divide rows into two groups to find two peak centers.
 meanRow = mean(rowCle);
-[row1,col1] = find(rowCle <= meanRow);
-[row2,col2] = find(rowCle >= meanRow);
-max1=0;
-for i = 1:size(row1,1)
-    a1=AA(rowCle(row1(i),col1(i)),ColCle(row1(i),col1(i)));
-    if a1>=max1
-        max1=a1;
-    end
-end
-[m1final,n1final]=find(AA == max1);
-max2=0;
-for i = 1:size(row2,1)
-    a2=AA(rowCle(row2(i),col2(i)),ColCle(row2(i),col2(i)));
-    if a2>=max2
-        max2=a2;
-    end
-end
-[m2final,n2final]=find(AA == max2);
-x = mean([m2final, m1final]);
-y = mean([n1final , n2final]);
-%  Center of ellipse
-pts = [x , y];
-% direction of ellipse
-Theta = [Theta1;Theta2;Theta3;Theta4];
-%% find dimension of Ellpise with projection of image
-R1 =  radon(f,cell2mat(Theta4(1,1)));  % Theta should be changed related to the degree
-R2 = radon(f,cell2mat(Theta4(1,1))+90);  % Theta should be changed related to the degree
-X1 = find(R1 ~= 0);
-big = (max(X1) - min(X1))/2; % big dimension
-X2 = find(R2 ~= 0);
-small = (max(X2) - min(X2))/2; % small dimension
-%%%
-Detected_Ellipse = Ellipse(50,20,cell2mat(Theta4(1,1)),x,y,512,512); % Theta should be changed related to the degree
-figure;imshow(f);hold on ; contour(Detected_Ellipse,'r')
+[row1, ~] = find(rowCle <= meanRow);
+[row2, ~] = find(rowCle > meanRow);
+
+% Identify the maximum coefficients in each group.
+[max1, idx1] = max(AA(rowCle(row1), colCle(row1)));
+[max2, idx2] = max(AA(rowCle(row2), colCle(row2)));
+
+% Calculate the ellipse center.
+center_x = mean([rowCle(idx1), rowCle(idx2)]);
+center_y = mean([colCle(idx1), colCle(idx2)]);
+ellipse_center = [center_x, center_y];
+
+% Estimate ellipse dimensions from projections.
+R1 = radon(f, cell2mat(Theta(4))); % -45 degree orientation.
+R2 = radon(f, cell2mat(Theta(4)) + 90); % Perpendicular direction.
+
+% Calculate major and minor axes.
+major_axis = (max(find(R1)) - min(find(R1))) / 2;
+minor_axis = (max(find(R2)) - min(find(R2))) / 2;
+
+%% Step 6: Overlay the Detected Ellipse
+Detected_Ellipse = Ellipse(major_axis, minor_axis, cell2mat(Theta(4)), ...
+                           ellipse_center(1), ellipse_center(2), 512, 512);
+
+figure; imshow(f); hold on;
+contour(Detected_Ellipse, 'r');
+title('Detected Ellipse');
